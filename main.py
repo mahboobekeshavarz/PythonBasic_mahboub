08ebdb9f7d02e2d7cb0e9b7883d8b39443b31f3b
mahboobekeshavarz <mahboobe_keshavarz@yahoo.com>
2023-08-02 20:03:14 +0330

    add file named "main.py"

---
+++ main.py
@@ -0,0 +1,21 @@
+# my_tuple = [12, 32, '123']
+
+# for index, elm in enumerate(my_tuple):
+#     if index == 2:
+#         # elm = 54
+#         my_tuple[2] = 54
+
+# print(my_tuple)
+
+# name1 = 'jina'
+# name2 = 'jina'
+# print(name1 is name2)
+
+# def Hello(name, age, active = True):
+#     return f'hello {name}', age
+
+# print(Hello('mahbob', 39))
+
+# print('hello')
+import pandas as pd
+data_frame = pd.read_excel('hala_harchi.xlsx')
